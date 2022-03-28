f832b0aa7767d643d81649b48be0e84e25bdf047
pith <ollyc88@gmail.com>
2022-03-28 21:32:10 +0100

    added hitbox

--- World/Grass.gd
+++ World/Grass.gd
@@ -8,14 +8,18 @@ extends Node2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://World/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
+func generate_grass():
+	var GrassEffect = load("res://World/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
 		
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
 		
-		grassEffect.global_position = global_position
+	grassEffect.global_position = global_position
 		
-		queue_free()
+
+
+func _on_HurtBox_area_entered(area):
+	generate_grass()
+	queue_free()
+	
